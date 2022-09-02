670675f947f4a3f662d9712c04fd00def4fdcfdb
Luis Gaeta <luigaeta@hotmail.com>
2022-09-02 18:09:32 -0500

    Se añade un nuevo problema de manipulación de bits en "blanco"

---
+++ Ejercicios/Hackerearth/5 - Manipulación de Bits/CountPairs.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	int num;
+	scanf("%d", &num);              	                  // Reading input from STDIN
+	printf("Input number is %d.\n", num);       // Writing output to STDOUT
+}
