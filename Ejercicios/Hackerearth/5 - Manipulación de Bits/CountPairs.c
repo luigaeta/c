067cce3e21bb89e04f9bea8c14eab94a186f8209
Luis Gaeta <luigaeta@hotmail.com>
2022-09-05 18:27:28 -0500

    Se modifica el programa, se encuentra un problema en la dirección de la variable donde se guardaran los datos

--- Ejercicios/Hackerearth/5 - Manipulación de Bits/CountPairs.c
+++ Ejercicios/Hackerearth/5 - Manipulación de Bits/CountPairs.c
@@ -2,12 +2,13 @@
 #define size==N
 
 int main(){
-int T, N,i,j,A;
+int T, N,i,j,z,k;
+k=0;
 scanf("%d",&T);
 for(i=0;i<T;i++)
 {
     scanf("%d",&N);
-    unsigned long long int** A;
+    unsigned long long int* A;
     A = (int*)malloc(N * sizeof(int));
     for(j=0;j<N;j++)
     {
