b47caee45411897b452c1ad3cd2975ac1f908bd7
Luis Gaeta <luigaeta@hotmail.com>
2022-09-05 19:08:52 -0500

    Se agrega el.exe

---
+++ Ejercicios/Hackerearth/5 - Manipulacion de Bits/Count Pairs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define size==N
+
+int main(){
+int T, N,i,j,A;
+scanf("%d",&T);
+for(i=0;i<T;i++)
+{
+    scanf("%d",&N);
+    unsigned long long int** A;
+    A = (int*)malloc(N * sizeof(int));
+    for(j=0;j<N;j++)
+    {
+        scanf("%d",&A[j]);
+    }
+}
+}  
