7e4688cdd14cc5d1f9bb83d253b8a984951eca35
Luis Gaeta <luigaeta@hotmail.com>
2022-09-06 17:20:28 -0500

    Se deja el problema "Count Pairs" pendiente, problema con la combinación de numeros

--- Ejercicios/Hackerearth/5 - Manipulacion de Bits/Count Pairs.c
+++ Ejercicios/Hackerearth/5 - Manipulacion de Bits/Count Pairs.c
@@ -1,29 +1,44 @@
 #include <stdio.h>
-
-int main(){
-    int T, N,i,j,k,z,s=0;
-    scanf("%d",&T);
-    for(i=0;i<T;i++)
+long int xor(long int a,long int b){
+    long int r,s=0;
+    r=a^b;
+    if(r>a&&r>b)
     {
-        scanf("%d",&N);
-        unsigned long long int A[sizeof(N)];
+        s+=1;
+    }    
+    return s;
+}
+int main(){
+    long int T, N,i,j,k,R;
+    scanf("%li",&T);
+    long int *Resultados;
+    Resultados = (long int*)malloc(T * sizeof(long int));
+    for(i=0;i<T;i++)
+    { 
+        
+        scanf("%li",&N);
+        long int *A;
+        A = (long int*)malloc(N * sizeof(long int));
         for(j=0;j<N;j++)
         {
-            scanf("%llu",&A[j]);
+            scanf("%li",&A[j]);
         }
+        R=0;
         for(j=0;j<N;j++)
         {
             for(k=j+1;k<N;k++)
             {
-                z=A[j]^A[k];
-                if(z>A[j]&&z>A[k])
+                if(xor(A[j],A[k]))
                 {
-                    s=s+1;
+                    R +=1;
                 }
 
             }
         }
+    Resultados[i] = R;
     }
-    printf("%d",s);
-    
+    for(i=0;i<T;i++)
+    { 
+    printf("%li\n",Resultados[i]);
+    }
 }  
