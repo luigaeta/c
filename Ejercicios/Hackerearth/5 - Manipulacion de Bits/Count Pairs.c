195c838231afeff05c8a5ffc1598561ee2ed8807
Luis Gaeta <luigaeta@hotmail.com>
2022-09-05 20:11:50 -0500

    Ejercicio a punto de completarse

--- Ejercicios/Hackerearth/5 - Manipulacion de Bits/Count Pairs.c
+++ Ejercicios/Hackerearth/5 - Manipulacion de Bits/Count Pairs.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    int T, N,i,j;
+    int T, N,i,j,k,z,s=0;
     scanf("%d",&T);
     for(i=0;i<T;i++)
     {
-        scanf("%i",&N);
+        scanf("%d",&N);
         unsigned long long int A[sizeof(N)];
         for(j=0;j<N;j++)
         {
             scanf("%llu",&A[j]);
         }
+        for(j=0;j<N;j++)
+        {
+            for(k=j+1;k<N;k++)
+            {
+                z=A[j]^A[k];
+                if(z>A[j]&&z>A[k])
+                {
+                    s=s+1;
+                }
+
+            }
+        }
     }
+    printf("%d",s);
     
 }  
